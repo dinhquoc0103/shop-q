3047fe9bf8ab8e19cf9c4642c7f44951c755d81d
quoc le <dinhquoc0103@gmail.com>
2023-02-08 13:32:38 +0700

    optimize filter list product

--- public/template/client/js/product.js
+++ public/template/client/js/product.js
@@ -4,45 +4,49 @@ $.ajaxSetup({
     }
 });
 
+/* Set localStorage url have not query string 
+    (Ex: http://127.0.0.1:8001/collection/men(right))
+    (Ex: http://127.0.0.1:8001/collection/men?page=&sortBy="price-asc"(wrong)) */
+function createUrl(queryParams) {
+    const params = new URLSearchParams(queryParams);
+    const defaultUrl = window.location.href.split('?')[0];
 
-$(document).ready(function (){
+    const newUrl = defaultUrl + "?" + params.toString();
+
+    return newUrl;
+}
+
+
+$(document).ready(function () {
     // Pagination without reload page
-    $(document).on("click", ".pagination a", function (e){
+    $(document).on("click", ".pagination a", function (e) {
         e.preventDefault();
-        
-        let url = $(this).attr("href");
-        let page = url.split("page=")[1];
-        let sortBy = $("select#collection-sorted-by").val();
-        let filterPrice = $("select#collection-filter-price").val();
+
+        const page = $(this).attr("href").split("page=")[1];
+        const sortBy = $("select#collection-sorted-by").val();
+        const filterPrice = $("select#collection-filter-price").val();
+
+        const queryParams = Object.assign({},
+            page > 0 ? { page } : null,
+            sortBy !== "default" ? { sortBy } : null,
+            filterPrice !== "default" ? { price: filterPrice } : null,
+        );
+
+        const url = createUrl(queryParams);
 
         // Fecth data by page
         $.ajax({
             type: "GET",
             dataType: "JSON",
-            url: url + "&sortBy=" + sortBy,
-            success: function (result){
+            url: url,
+            success: function (result) {
                 $("#list-product").html(result.htmlProductsPagination);
-                
-                if(sortBy !== "default" && filterPrice !== "default"){
-                    history.pushState({page: page}, '', "?page="+page+"&sortBy="+sortBy+"&price="+filterPrice)
-                }
-                else if(sortBy !== "default" && filterPrice == "default"){
-                    history.pushState({page: page}, '', "?page="+page+"&sortBy="+sortBy)
-                }
-                else if(sortBy == "default" && filterPrice !== "default"){
-                    history.pushState({page: page}, '', "?page="+page+"&price="+filterPrice)
-                }
-                else{
-                    // Save current url + parameter page (I do it because pagination ajax have not ?page=... )
-                    history.pushState({page: page}, '', "?page="+page)
-                }
 
-                // Go to or replace this url in history to the url on chrome
-                // history.go(page);
+                window.history.pushState({}, '', url);
 
-                $("html, body").animate({scrollTop: 50}, 1200);
+                $("html, body").animate({ scrollTop: 50 }, 1200);
             },
-            error: function (xhr, status, error){
+            error: function (xhr, status, error) {
                 console.log(error);
             }
         });
@@ -50,82 +54,54 @@ $(document).ready(function (){
 
 
     // Sorted by
-    $("select#collection-sorted-by").change(function() {
-        let sortBy = $(this).val();
-        let filterPrice = $("select#collection-filter-price").val();
-        var url = setDefaultUrl();
-        if(filterPrice !== "default"){
-            if(sortBy !== "default"){
-                url = url + "?page=1&sortBy=" + sortBy + "&price=" + filterPrice;
-                history.pushState({page: 1}, '', "?page=1&sortBy=" + sortBy + "&price=" + filterPrice);
-            }
-            else{
-                url = url + "?page=1&price=" + filterPrice;
-                history.pushState({page: 1}, '', "?page=1&price=" + filterPrice);
-            }
-        }
-        else{
-            if(sortBy !== "default"){
-                url = url + "?page=1&sortBy=" + sortBy;
-                history.pushState({page: 1}, '', "?page=1&sortBy=" + sortBy);
-            }
-            else{
-                url = url + "?page=1";
-                history.pushState({page: 1}, '', "?page=1");
-            }
-        } 
+    $("select#collection-sorted-by").change(function (e) {
+        const sortBy = $(this).val();
+        const filterPrice = $("select#collection-filter-price").val();
+
+        const queryParams = Object.assign({},
+            { page: 1 },
+            sortBy !== "default" ? { sortBy } : null,
+            filterPrice !== "default" ? { price: filterPrice } : null,
+        );
+
+        const url = createUrl(queryParams);
 
         $.ajax({
             type: "GET",
-            dataType: "JSON",   
+            dataType: "JSON",
             url: url,
-            success: function (result){
+            success: function (result) {
                 $("#list-product").html(result.htmlProductsPagination);
+                window.history.pushState({}, '', url);
             },
-            error: function (xhr, status, error){
+            error: function (xhr, status, error) {
                 console.log(error);
             }
         });
     });
 
     // Filter price
-    $("select#collection-filter-price").change(function (){
-        let filterPrice = $(this).val();
-        let sortBy = $("select#collection-sorted-by").val();
-        var url = setDefaultUrl();
+    $("select#collection-filter-price").change(function () {
+        const filterPrice = $(this).val();
+        const sortBy = $("select#collection-sorted-by").val();
 
-        if(sortBy !== "default"){            
-            if(filterPrice !== "default"){
-                url = url + "?page=1&sortBy=" + sortBy + "&price=" + filterPrice;
-                history.pushState({page: 1}, '', "?page=1&sortBy=" + sortBy + "&price=" + filterPrice);
-            }
-            else{
-                url = url + "?page=1&sortBy=" + sortBy;
-                history.pushState({page: 1}, '', "?page=1&sortBy=" + sortBy);
-            }
-        }
-        else{
-       
-            if(filterPrice !== "default"){
-                url = url + "?page=1&price=" + filterPrice;
-                history.pushState({page: 1}, '', "?page=1&price=" + filterPrice);
-            }
-            else{
-                url = url + "?page=1"
-                history.pushState({page: 1}, '',"?page=1");
-            }
-        }  
-        
+        const queryParams = Object.assign({},
+            { page: 1 },
+            filterPrice !== "default" ? { price: filterPrice } : null,
+            sortBy !== "default" ? { sortBy } : null,
+        );
+
+        const url = createUrl(queryParams);
+
         $.ajax({
             type: "GET",
             dataType: "JSON",
             url: url,
-            success: function (result){
-                console.log(result);
+            success: function (result) {
                 $("#list-product").html(result.htmlProductsPagination);
-                // history.go(1);
+                window.history.pushState({}, '', url);
             },
-            error: function (xhr, status, error){
+            error: function (xhr, status, error) {
                 console.log(error);
             }
 
@@ -133,10 +109,3 @@ $(document).ready(function (){
     });
 });
 
-/* Set localStorage url have not query string 
-    (Ex: http://127.0.0.1:8001/collection/men(right))
-    (Ex: http://127.0.0.1:8001/collection/men?page=&sortBy="price-asc"(wrong)) */
-function setDefaultUrl(){
-    let url = window.location.href;
-    return url.split('?')[0];
-}
