3705c630a82d47f22a89b6d116a6bd7c573d159e
quoc le <dinhquoc0103@gmail.com>
2023-02-08 15:54:56 +0700

    minify and optimize css

--- resources/views/client/components/head.blade.php
+++ resources/views/client/components/head.blade.php
@@ -1,4 +1,4 @@
-<title>{{ (isset($title)) ? $title : "Page Not Found" }}</title>
+<title>{{ isset($title) ? $title : 'Page Not Found' }}</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->
@@ -12,30 +12,26 @@
 <!--===============================================================================================-->
 <link rel="stylesheet" type="text/css" href="/template/client/fonts/linearicons-v1.0.0/icon-font.min.css">
 <!--===============================================================================================-->
-<link rel="stylesheet" type="text/css" href="/template/client/vendor/animate/animate.css">
+{{-- <link rel="stylesheet" type="text/css" href="/template/client/vendor/animate/animate.css"> --}}
 <!--===============================================================================================-->
 <link rel="stylesheet" type="text/css" href="/template/client/vendor/css-hamburgers/hamburgers.min.css">
 <!--===============================================================================================-->
-<link rel="stylesheet" type="text/css" href="/template/client/vendor/animsition/css/animsition.min.css">
+{{-- <link rel="stylesheet" type="text/css" href="/template/client/vendor/animsition/css/animsition.min.css"> --}}
 <!--===============================================================================================-->
-<link rel="stylesheet" type="text/css" href="/template/client/vendor/select2/select2.min.css">
+{{-- <link rel="stylesheet" type="text/css" href="/template/client/vendor/select2/select2.min.css"> --}}
 <!--===============================================================================================-->
-<link rel="stylesheet" type="text/css" href="/template/client/vendor/daterangepicker/daterangepicker.css">
+{{-- <link rel="stylesheet" type="text/css" href="/template/client/vendor/daterangepicker/daterangepicker.css"> --}}
 <!--===============================================================================================-->
 <link rel="stylesheet" type="text/css" href="/template/client/vendor/slick/slick.css">
 <!--===============================================================================================-->
-<link rel="stylesheet" type="text/css" href="/template/client/vendor/MagnificPopup/magnific-popup.css">
+{{-- <link rel="stylesheet" type="text/css" href="/template/client/vendor/MagnificPopup/magnific-popup.css"> --}}
 <!--===============================================================================================-->
-<link rel="stylesheet" type="text/css" href="/template/client/vendor/perfect-scrollbar/perfect-scrollbar.css">
+{{-- <link rel="stylesheet" type="text/css" href="/template/client/vendor/perfect-scrollbar/perfect-scrollbar.css"> --}}
 <!--===============================================================================================-->
 <link rel="stylesheet" href="/template/client/fonts/fontawesome-free/css/all.min.css">
 <!--===============================================================================================-->
-<link rel="stylesheet" type="text/css" href="/template/client/css/util.css">
-<link rel="stylesheet" type="text/css" href="/template/client/css/main.css">
+<link rel="stylesheet" type="text/css" href="/template/client/css/util.min.css">
+<link rel="stylesheet" type="text/css" href="/template/client/css/main.min.css">
 <!--===============================================================================================-->
 
 <meta name="csrf-token" content="{{ csrf_token() }}">
-
-
-
-
